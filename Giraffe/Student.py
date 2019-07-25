068e8289abd75e0e5574d5307e718593efee3cba
Jerry <529008115@qq.com>
2019-07-25 16:44:03 +0800

    Deleted Giraffe

--- Giraffe/Student.py
+++
@@ -1,9 +0,0 @@
-from student import Student
-
-student1 = Student("Tom", "Chemistry", 3.5, False)
-student2 = Student("Sally", "Physics", 2.5, True)
-print(student1.is_on_probation)
-print(student1.name)
-print(student2.major)
-print(student1.on_honor_roll())
-print(student2.on_honor_roll())
--- Giraffe/app02.py
+++
@@ -1,7 +0,0 @@
-coordinates = (4, 5)
-
-print(coordinates[0])
-print(coordinates[-1])
-
-coordinates_list = [(4, 5), (3, 6), (2, 7)]
-print(coordinates_list)
